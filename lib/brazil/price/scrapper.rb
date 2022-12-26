6c3db7fa57dcce7d1e6510600ae559e2eeed3793
Gabriel <gabrielgomesch@gmail.com>
2022-12-26 19:51:51 -0300

    initial commit

---
+++ lib/brazil/price/scrapper.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "scrapper/version"
+
+module Brazil
+  module Price
+    module Scrapper
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
